8792a6becfc3e61a4be59aa911e7701c2953f957
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2024-01-10 19:01:55 +0100

    Poprawienie rozwiązania.

--- src/main/java/pl/javastart/task/BubbleSort.java
+++ src/main/java/pl/javastart/task/BubbleSort.java
@@ -5,19 +5,25 @@ public class BubbleSort {
     // ta metoda powinna sortować inputArray oraz zwracać informację o liczbie porównań, które były do tego potrzebne
     public int sort(int[] inputArray) {
         int n = inputArray.length;
-        int count = 0;
+        boolean swapped;
+        int comparisons = 0;
 
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
-                count++;
+                comparisons++;
                 if (inputArray[j] > inputArray[j + 1]) {
-                    // Zamiana miejscami gdy wartość jest większa
                     int temp = inputArray[j];
                     inputArray[j] = inputArray[j + 1];
                     inputArray[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            // Jeśli w jednym przejściu pętli nie było żadnej zamiany, to tablica jest już posortowana
+            if (!swapped) {
+                break;
+            }
         }
-        return count;
+        return comparisons;
     }
 }
